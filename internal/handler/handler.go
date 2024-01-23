dae13c2921138940df2321efb1bd7c4bd3d4f0d5
Ислам <114616202+Gshot05@users.noreply.github.com>
2024-01-23 10:53:34 +0300

    Update handler.go

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"encoding/json"
+	"log"
 	"myapp/database"
 	"myapp/internal/service"
 	"net/http"
@@ -9,13 +10,16 @@ import (
 )
 
 func HandleRequest(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
+	switch r.Method {
+	case http.MethodPost:
 		handlePost(w, r)
-	} else if r.Method == http.MethodDelete {
+	case http.MethodDelete:
 		HandleDelete(w, r)
-	} else if r.Method == http.MethodGet {
+	case http.MethodGet:
 		HandleGetPeople(w, r)
-	} else {
+	case http.MethodPut:
+		HandleUpdatePerson(w, r)
+	default:
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
 }
@@ -26,18 +30,21 @@ func handlePost(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&person)
 	if err != nil {
+		log.Println("Error decoding request payload:", err)
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
 
 	err = service.EnrichPersonData((*service.Person)(&person))
 	if err != nil {
+		log.Println("Error enriching data:", err)
 		http.Error(w, "Error enriching data", http.StatusInternalServerError)
 		return
 	}
 
 	err = database.InsertPerson(&person)
 	if err != nil {
+		log.Println("Error inserting data into database:", err)
 		http.Error(w, "Error inserting data into database", http.StatusInternalServerError)
 		return
 	}
@@ -51,12 +58,14 @@ func HandleDelete(w http.ResponseWriter, r *http.Request) {
 		idStr := r.URL.Query().Get("id")
 		id, err := strconv.Atoi(idStr)
 		if err != nil {
+			log.Println("Invalid ID parameter:", err)
 			http.Error(w, "Invalid ID", http.StatusBadRequest)
 			return
 		}
 
 		err = service.DeletePersonByID(id)
 		if err != nil {
+			log.Println("Error deleting data from database:", err)
 			http.Error(w, "Error deleting data from database", http.StatusInternalServerError)
 			return
 		}
@@ -72,18 +81,53 @@ func HandleGetPeople(w http.ResponseWriter, r *http.Request) {
 		limitStr := r.URL.Query().Get("limit")
 		limit, err := strconv.Atoi(limitStr)
 		if err != nil {
+			log.Println("Invalid limit parameter:", err)
 			http.Error(w, "Invalid limit parameter", http.StatusBadRequest)
 			return
 		}
 
 		people, err := service.GetPeopleWithLimit(limit)
 		if err != nil {
+			log.Println("Error selecting data from database:", err)
 			http.Error(w, "Error selecting data from database", http.StatusInternalServerError)
 			return
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(people)
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
+func HandleUpdatePerson(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPut {
+		var updatedPerson database.Person
+
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&updatedPerson)
+		if err != nil {
+			log.Println("Error decoding request payload:", err)
+			http.Error(w, "Invalid request payload", http.StatusBadRequest)
+			return
+		}
+
+		idStr := r.URL.Query().Get("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			log.Println("Invalid ID parameter:", err)
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
+			return
+		}
+
+		err = service.UpdatePerson(id, ((*service.Person)(&updatedPerson)))
+		if err != nil {
+			log.Println("Error updating data in the database:", err)
+			http.Error(w, "Error updating data in the database", http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
