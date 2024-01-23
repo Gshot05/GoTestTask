d3b5a50878990c1f6922a86cb496578830d95e90
Ислам <114616202+Gshot05@users.noreply.github.com>
2024-01-23 10:53:10 +0300

    Update db.go

--- database/db.go
+++ database/db.go
@@ -15,7 +15,7 @@ var db *sql.DB
 func Init() (*sql.DB, error) {
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("Error loading .env file")
+		log.Println("Error loading .env file:", err)
 		os.Exit(1)
 	}
 
@@ -30,12 +30,12 @@ func Init() (*sql.DB, error) {
 
 	db, err = sql.Open("postgres", connectionString)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error connecting to the database:", err)
 	}
 
 	err = db.Ping()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error pinging the database:", err)
 	}
 
 	return db, nil
@@ -47,6 +47,10 @@ func InsertPerson(person *Person) error {
         VALUES ($1, $2, $3, $4, $5, $6);
     `, person.FirstName, person.LastName, person.Patronymic, person.Gender, person.Age, person.Country)
 
+	if err != nil {
+		log.Println("Error inserting person into the database:", err)
+	}
+
 	return err
 }
 
@@ -54,6 +58,10 @@ func DeletePersonByID(id int) error {
 	_, err := db.Exec(`
 		DELETE FROM people WHERE id = $1;
 	`, id)
+
+	if err != nil {
+		log.Println("Error deleting person from the database:", err)
+	}
 
 	return err
 }
@@ -64,6 +72,7 @@ func SelectPeople(limit int) ([]Person, error) {
 		LIMIT $1;
 	`, limit)
 	if err != nil {
+		log.Println("Error selecting people from the database:", err)
 		return nil, err
 	}
 	defer rows.Close()
@@ -80,10 +89,25 @@ func SelectPeople(limit int) ([]Person, error) {
 			&person.Country,
 		)
 		if err != nil {
+			log.Println("Error scanning row:", err)
 			return nil, err
 		}
 		people = append(people, person)
 	}
 
 	return people, nil
+}
+
+func UpdatePerson(id int, updatedPerson *Person) error {
+	_, err := db.Exec(`
+        UPDATE people
+        SET first_name=$1, last_name=$2, patronymic=$3, gender=$4, age=$5, country=$6
+        WHERE id=$7;
+    `, updatedPerson.FirstName, updatedPerson.LastName, updatedPerson.Patronymic, updatedPerson.Gender, updatedPerson.Age, updatedPerson.Country, id)
+
+	if err != nil {
+		log.Println("Error updating person in the database:", err)
+	}
+
+	return err
 }
